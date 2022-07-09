108cc1e66d8a97ff856f5886a4f0a1c6c6dea5b8
Humorousrock <86119417+Humorousrock@users.noreply.github.com>
2022-07-10 01:39:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# React-Portfolio made with love and with beautiful UI.
+
+
+
+### Hi there 👋 I am Aman, a full stack web and app developer. I love to build new things that excites me a lot.
+
+
+
+
+
---
+++ src/components/skills/Interest.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import classes from './Interest.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+
+class Interest extends Component {
+  render() {
+    return (
+      <div className={classes.box} id="interest">
+        <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+          <span className={classes.head}>WHAT I DO?</span>
+          <h2 className={classes.heading}>HERE ARE SOME OF MY EXPERTISE</h2>
+          <div className={classes.Interest}>
+            <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+              <div className={classes.web}>
+                <h3>WEB Develpoment</h3>
+                <p>I also have knowledge of React js and Node js and have experience in building Food ordering System . </p>
+              </div>
+            </ScrollAnimation>
+            <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+              <div className={classes.app}>
+                <h3>Programmer</h3>
+                <p>I am have a good coding experience in c++ language i had completed 100+ question in leetcode<br /> </p>
+              </div>
+            </ScrollAnimation>
+            <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+              <div className={classes.other}>
+                <h3>Other's Interest</h3>
+                <p>I also have  good grasp on  SQl<br />, Github are my other fields of interest. Currently, I am investing my free time in REST Api </p>
+              </div>
+            </ScrollAnimation>
+          </div>
+        </ScrollAnimation>
+      </div>
+    )
+  }
+}
+
+export default Interest;
